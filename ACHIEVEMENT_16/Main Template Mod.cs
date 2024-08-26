0c108665428f33d7ad72b65dfbc39a21f83fe9f9
Phan <Phan@WF>
2024-08-26 18:29:28 +0800

    Minimum viable harmony patch

--- ACHIEVEMENT_16/Main Template Mod.cs
+++ ACHIEVEMENT_16/Main Template Mod.cs
@@ -1,13 +1,10 @@
-﻿using Deadpan.Enums.Engine.Components.Modding;
-
-public class TemplateMod : WildfrostMod
-    {
-    [HarmonyLib.HarmonyPatch(typeof(CheckAchievements), "Start")]
-    class PatchAchievements { static bool Prefix() => false; }
-    public TemplateMod(string modDirectory) : base(modDirectory) { }
-        public override string GUID => "hope.wildfrost.ACHIEVEMENT_16";
-        public override string[] Depends => [];
-        public override string Title => "ACHIEVEMENT_16";
-        public override string Description => "Here's your ACHIEVEMENT_16";
-    }
-
+﻿using Deadpan.Enums.Engine.Components.Modding; // Assembly-CSharp.dll
+[HarmonyLib.HarmonyPatch(typeof(CheckAchievements), "Start")] // 0Harmony.dll
+public class TemplateMod(string modDirectory) : WildfrostMod(modDirectory)
+{
+    static bool Prefix() => false;
+    public override string GUID => "hope.wildfrost.ACHIEVEMENT_16";
+    public override string[] Depends => [];
+    public override string Title => "ACHIEVEMENT_16";
+    public override string Description => "Here's your ACHIEVEMENT_16";
+}
